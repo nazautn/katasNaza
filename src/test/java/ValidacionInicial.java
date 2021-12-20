549695e4c94f9c9d95bc0dd4af9ed5d066ab5092
Naza <naza16@gmail.com>
2021-12-20 11:48:22 -0300

    Commit inicial

---
+++ src/test/java/ValidacionInicial.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ValidacionInicial {
+
+    @Test
+    public void trueDeberiaSerTrue() {
+        assertTrue(true);
+    }
+}
